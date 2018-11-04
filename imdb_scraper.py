ef06bd3da361f22e7d138ba153ade931007e089c
Mario Schuetz <mayo2@gmx.de>
2018-11-04 17:36:00 +0100

    Fetch data from imdb calendar and filter for movies

---
+++ imdb_scraper.py
@@ -0,0 +1,29 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = 'http://www.imdb.com/calendar?region=DE&ref_=rlm'
+
+# download URL and extract content
+request = urllib.request.Request(url)
+html = urllib.request.urlopen(request).read()
+
+# pass HTML to BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
+calendar = soup.find('div', attrs={'id':'main'})
+
+dates = calendar.find_all('h4')
+links = calendar.find_all('a')
+
+# from each link extract text of link and link itself
+upcoming_movies = []
+for link in links:
+	title = link.text
+	url = link['href']
+	movie = {
+		'title' :title,
+		'url' :url
+	}
+	upcoming_movies.append(movie)
+
+print (upcoming_movies)
+
