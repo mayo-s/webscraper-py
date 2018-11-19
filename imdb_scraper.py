5ad0d06ce4121817a7e0d16b538fb0c411ecff88
Mario Schuetz <mayo2@gmx.de>
2018-11-19 17:24:02 +0100

    Correct typos and value assignment

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -1,29 +1,33 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import json
+import re
 
 initial_url = 'http://www.imdb.com/calendar?region=DE&ref_=rlm'
 
-def soup_maker(url){
+def soup_maker(url):
 # download URL and extract content
-	request = urllib.request.Request(initial_url)
+	request = urllib.request.Request(url)
 	html = urllib.request.urlopen(request).read()
 
 	# pass HTML to BeautifulSoup
 	soup = BeautifulSoup(html, 'html.parser')
 	return soup
-}
 
 # extract specific movie information
 def parse_movie(url):
 	soup = soup_maker(url)
-	info = soup.find('div', attrs={'class':'subtext'})
-	genre = info.find_all('a', attrs={'href':'genre'})
+	info_box = soup.find('div', attrs={'class':'title_wrapper'})
+	info = info_box.find('div', attrs={'class':'subtext'})
 	length = info.find('time').text
-	release_date = info.find('a', attrs={'title':'release'}).text
-	
+	release_date = info.find('a', attrs={'title':'See more release dates'}).text
+	genres = info.select('a[href*=genres]')	
+	genre_text = []
+	for genre in genres:
+		text = genre.text
+		genre_text.append(text)
 	data = {
-		'genre':genre,
+		'genres':genre_text,
 		'release_date':release_date,
 		'length':length
 	}
@@ -36,18 +40,18 @@ links = calendar.find_all('a')
 
 # from each link extract text of link and link itself
 upcoming_movies = []
-for link in links:
+for link in links[:10]:
 	title = link.text
 	url = link['href']
 	if not url.startswith('http'):
 		url = 'https://www.imdb.com' + url
-	info = parse_movie(url))
+	info = parse_movie(url)
 	movie = {
 		'title' :title,
 		'url' :url,
-		'genre' :info.genre,
-		'length' :info.length,
-		'release_date' : info.release_date
+		'genres' :info.get('genres'),
+		'length' :info.get('length'),
+		'release_date' : info.get('release_date')
 	}
 	upcoming_movies.append(movie)
 
@@ -57,6 +61,7 @@ with open('imdb_data.json', 'w') as outfile:
 
 # print scraped data
 for movie in upcoming_movies:
+	print('')
 	print(movie)
 
 
