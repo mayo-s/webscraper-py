439435ca7cd1017f030960d926194fac8e07cd50
Mario Schuetz <mayo2@gmx.de>
2018-11-19 20:46:36 +0100

    Beautify scraped data and console print out

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -1,8 +1,9 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import json
-import re
+import time
 
+start = time.time()
 initial_url = 'http://www.imdb.com/calendar?region=DE&ref_=rlm'
 
 def soup_maker(url):
@@ -17,19 +18,20 @@ def soup_maker(url):
 # extract specific movie information
 def parse_movie(url):
 	soup = soup_maker(url)
-	info_box = soup.find('div', attrs={'class':'title_wrapper'})
-	info = info_box.find('div', attrs={'class':'subtext'})
-	length = info.find('time').text
-	release_date = info.find('a', attrs={'title':'See more release dates'}).text
-	genres = info.select('a[href*=genres]')	
+	title_wrapper= soup.find('div', attrs={'class':'title_wrapper'})
+	subtext = title_wrapper.find('div', attrs={'class':'subtext'})
+	# not all movies have a specified length
+	# length = subtext.find('time').text
+	release_date = subtext.find('a', attrs={'title':'See more release dates'}).text
+	genres = subtext.select('a[href*=genres]')	
 	genre_text = []
 	for genre in genres:
 		text = genre.text
-		genre_text.append(text)
+		genre_text.append(text[1:-1])
 	data = {
 		'genres':genre_text,
-		'release_date':release_date,
-		'length':length
+		'release_date':release_date[:16],
+	#	'length':length
 	}
 	return data
 
@@ -40,18 +42,18 @@ links = calendar.find_all('a')
 
 # from each link extract text of link and link itself
 upcoming_movies = []
-for link in links[:10]:
+for link in links:
 	title = link.text
 	url = link['href']
 	if not url.startswith('http'):
 		url = 'https://www.imdb.com' + url
 	info = parse_movie(url)
 	movie = {
-		'title' :title,
-		'url' :url,
-		'genres' :info.get('genres'),
-		'length' :info.get('length'),
-		'release_date' : info.get('release_date')
+		'title':title,
+		'url':url,
+		'genres':info.get('genres'),
+	#	'length':info.get('length'),
+		'release_date':info.get('release_date')
 	}
 	upcoming_movies.append(movie)
 
@@ -59,9 +61,26 @@ for link in links[:10]:
 with open('imdb_data.json', 'w') as outfile:
 	json.dump(upcoming_movies, outfile, indent=4)
 
+def time_format(num):
+    return ('%.1f' % num).rstrip('0').rstrip('.')
+
 # print scraped data
+print ('')
+print ('#################################')
+print ('####      IMDb Scraper       ####')
+text = '#### Movies scraped:    ' + str(len(upcoming_movies)) + '   ####'
+print (text)
+end = time.time()
+time = time_format(end - start)
+text = '#### Time needed: ' + str(time) + ' secs ####'
+print (text)
+print ('#################################')
+
 for movie in upcoming_movies:
 	print('')
-	print(movie)
+	print(movie.get('title'))
+	print(movie.get('url'))
+	print(movie.get('genres'))
+	print(movie.get('release_date'))
 
 
