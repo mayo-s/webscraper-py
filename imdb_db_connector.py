f008c35bcc2c23a911b483456fa69e849e81c21d
Mario Schuetz <mayo2@gmx.de>
2018-11-27 15:27:49 +0100

    Add method to SELECT *. Dump to json (still failing)

--- imdb_db_connector.py
+++ imdb_db_connector.py
@@ -2,13 +2,14 @@ import mysql.connector
 import datetime
 import json
 
+# connect to local db
 db = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="",
     database="imdb"
     )   
-dbc = db.cursor()
+dbc = db.cursor(named_tuple=True)
 
 # INSERT new movie dataset
 def insert_movie(title, imdb_id, url, release_date):
@@ -17,17 +18,17 @@ def insert_movie(title, imdb_id, url, release_date):
     try:
         dbc.execute(sql, val)
         db.commit()
-        print ("INSERT successful")
     except mysql.connector.errors.OperationalError:
         print("Error: INSERT movie")
 
+# Read JSON file
 def json_read(filename):
     with open(filename) as movies:
         data = json.load(movies)
     return data
 
-# INSERT all movies from scraped json file
-def insert_movies():
+# INSERT all movies from scraped JSON file
+def insert_all_movies():
     upcoming_movies = json_read('imdb_data.json')
     for movie in upcoming_movies:
         title = movie.get('title')
@@ -36,8 +37,30 @@ def insert_movies():
         # release_date.get('release_date')
         release_date = datetime.datetime(2037,3,1) # dummy date
         insert_movie(title, imdb_id, url, release_date)
+    print ("INSERT successful")
 
-insert_movies()
+# JSON dump
+def json_dump(data):
+    movies = []
+    for movie in data:
+        movies.append({'id':movie.id, 'title':movie.title, 'imdb_id':movie.imdb_id, 'url':movie.url, 'release_date':movie.release_date, 'ranking':movie.ranking})
+    with open('imdb_db_data.json', 'w') as outfile:
+        json.dump(movies, outfile, indent=4)
+    print ("JSON dump successful")
+
+# SELECT all movies database
+def select_all():
+    try:
+        data = dbc.execute("SELECT * FROM movies")
+        print ("SELECT * FROM movies successful")
+        print ("JSON dump in progress... ")
+        json_dump(data)
+        print ("JSON dump completed")
+    except mysql.connector.errors.OperationalError:
+        print("Error: SELECT * ")
+
+# insert_all_movies()
+select_all()
 
 db.close()
 
